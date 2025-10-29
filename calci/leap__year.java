7ab9d9bd44c4585cd4a71b0cf079ce47447b84c9
coderock100 <vd.ssaathvik@gmail.com>
2025-10-29 11:15:35 +0530

    calci

---
+++ calci/leap__year.java
@@ -0,0 +1,16 @@
+public class leap__year {
+    import java.util.Scanner;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a year: ");
+        int year = sc.nextInt();
+
+        String result = (year % 400 == 0) ? "Leap Year" 
+                       : (year % 100 == 0) ? "Not a Leap Year" 
+                       : (year % 4 == 0) ? "Leap Year" 
+                       : "Not a Leap Year";
+
+        System.out.println(result);
+    }
+}
+
