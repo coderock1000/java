ef9feeec26b841cec6eaf51d83fa0d1599d9491c
coderock100 <vd.ssaathvik@gmail.com>
2025-11-04 21:20:31 +0530

    statastics

--- Control Structures and Arrays/Reverse_numbers.java
+++ Control Structures and Arrays/Reverse_numbers.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ReverseNumber {
+public class Reverse_numbers {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
@@ -17,5 +17,6 @@ public class ReverseNumber {
         System.out.println("Reversed number: " + reversed);
         
         sc.close();
+        
     }
 }
