25260ed5f1b167f530d5d9cc51ca5dee9a91292b
coderock100 <vd.ssaathvik@gmail.com>
2025-11-17 19:04:30 +0530

    Volume

---
+++ Get Set Go/VolumeMain.java
@@ -0,0 +1,27 @@
+ class Volume {
+    private double length;
+    private double width;
+    private double height;
+
+    public void setDimensions(double l, double w, double h) {
+        length = l;
+        width = w;
+        height = h;
+    }
+
+    public double getVolume() {
+        return length * width * height;
+    }
+}
+
+public class VolumeMain {
+    public static void main(String[] args) {
+        Volume v = new Volume();
+
+        v.setDimensions(5, 4, 3); 
+
+        System.out.println("Volume = " + v.getVolume());
+    }
+} 
+    
+
